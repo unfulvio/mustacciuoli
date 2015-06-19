ff6ef2eb237e48f62217776cee9e05f693b90e14
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2015-06-19 13:32:05 +0200

    PHP 5.4 required

--- mustacciuoli.php
+++ mustacciuoli.php
@@ -18,7 +18,48 @@
  * Domain Path:       /languages
  */
 
-if ( ! defined( 'WPINC' ) ) die;
+/**
+ * Mustacciuoli
+ * Copyright (c) 2014-2015 Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
+ * and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	exit; // Exit if accessed directly
+}
+
+/**
+ * Mustacciuoli PHP 5.4.0 minimum.
+ * WordPress supports 5.2.4 and recommends 5.4.0.
+ * @link https://make.wordpress.org/plugins/2015/06/05/policy-on-php-versions/
+ */
+if ( version_compare( PHP_VERSION, '5.4.0', '<') ) {
+	add_action( 'admin_notices',
+		function() {
+			echo '<div class="error"><p>'.
+			     sprintf( __( "Mustacciuoli requires PHP 5.4 or above to function properly. Detected PHP version on your server is %s. Please upgrade PHP to activate Mustacciuoli or remove the plugin.", 'mustacciuoli' ), phpversion() ? phpversion() : '`undefined`' ) .
+			     '</p></div>';
+			if ( isset( $_GET['activate'] ) ) {
+				unset( $_GET['activate'] );
+			}
+		}
+	);
+	return;
+}
 
 /**
  * Include Mustache.php library.
